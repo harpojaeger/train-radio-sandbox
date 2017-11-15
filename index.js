49e7700359dd985f0039e7ac59b2fa58478f0d0a
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-11-15 18:05:17 -0500

    Try out the point-to-line-snapper

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const myLine = require('./myLine.json')
 
 const bkCoords = [-73.9585526, 40.6764563]
 
-const distance = turf.pointToLineDistance(myCoords, myLine, {units: 'miles'})
-
+const distance = turf.pointToLineDistance(bkCoords, myLine, {units: 'miles'})
 console.log(distance)
+
+const closestPoint = turf.nearestPointOnLine(myLine, bkCoords)
+console.log(closestPoint)
