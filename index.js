5bbb7a3b6d199150d5a82fee3a6c5bdb62ddc304
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-11-15 18:20:39 -0500

    Working distance calculator

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const myLine = require('./myLine.json')
 
 const bkCoords = [-73.9585526, 40.6764563]
 
-const distance = turf.pointToLineDistance(myCoords, myLine, {units: 'miles'})
-
+const distance = turf.pointToLineDistance(bkCoords, myLine, {units: 'miles'})
 console.log(distance)
+
+const nearestPoint = turf.nearestPointOnLine(myLine, bkCoords, {units: 'miles'})
+console.log(nearestPoint)
