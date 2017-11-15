f6a9afebfff5ec230d9be6b40750893fd40c7057
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-11-15 18:09:54 -0500

    Revert "Try out the point-to-line-snapper"
    This reverts commit 49e7700359dd985f0039e7ac59b2fa58478f0d0a.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const myLine = require('./myLine.json')
 
 const bkCoords = [-73.9585526, 40.6764563]
 
-const distance = turf.pointToLineDistance(bkCoords, myLine, {units: 'miles'})
-console.log(distance)
+const distance = turf.pointToLineDistance(myCoords, myLine, {units: 'miles'})
 
-const closestPoint = turf.nearestPointOnLine(myLine, bkCoords)
-console.log(closestPoint)
+console.log(distance)
