4324bea19ae15a6a6a921a53c8d920e8905d5b8d
Harpo Jaeger <harpo.jaeger@gmail.com>
2017-12-24 13:27:02 -0500

    Making some progress w/ formats, but still getting bad numbers for distances

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const myLine = require('./myLine.json')
 const texasEagleAllFeatures = require('./texaseagle.json')
 const texasEagleStripped = require('./texaseagle_stripped.json')
+const texasEagleDissolved = require('./texaseagle-dissolved-ls.json')
 
 // Some simple tests to make sure it's configured right. Interestingly, the return values of pointToLineDistance and nearestPointOnLine are off by just a little bit (~1.5 miles, probably not enough to matter at the moment).
 // const bkCoords = [-73.9585526, 40.6764563]
@@ -15,17 +16,18 @@ const texasEagleStripped = require('./texaseagle_stripped.json')
 const SPIStation = [-89.6518184, 39.8023754]
 
 // console.log('texas eagle stripped prop names:', Object.getOwnPropertyNames(texasEagleStripped))
-console.log('texas eagle has', texasEagleStripped.features.length,'features')
+// console.log('texas eagle has', texasEagleStripped.features.length,'features')
 
-const texasEagleWays = texasEagleStripped.features.reduce((acc, val) => {
-  return [...acc,...val.geometry.coordinates]
-},[])
+// const texasEagleWays = texasEagleStripped.features.reduce((acc, val) => {
+//   return [...acc,...val.geometry.coordinates]
+// },[])
 
 // console.log(texasEagleWays)
 
-console.log('texas eagle stripped route length:', turf.length(turf.lineString(texasEagleWays, {units: "miles"})))
+console.log('texas eagle stripped route length:', turf.length(texasEagleDissolved))
 
 
-// const nearestPointToSPI = turf.nearestPointOnLine(turf.lineString(texasEagleStripped), SPIStation, {units: 'miles'})
-//Springfield station should be at MP ~185.1. This fails, possibly because turf.featureReduce is putting the points out of order? That would cause turf.nearestPointOnLine to think it's a lot farther to SPI than it really is.
-// console.log(nearestPointToSPI)
+const nearestPointToSPI = turf.nearestPointOnLine(texasEagleDissolved, SPIStation, {units: 'miles'})
+// Springfield station should be at MP ~185.1. This fails, possibly because turf.featureReduce is putting the points out of order? That would cause turf.nearestPointOnLine to think it's a lot farther to SPI than it really is.
+console.log(nearestPointToSPI)
+console.log(turf.length(texasEagleDissolved)-nearestPointToSPI.properties.location)
